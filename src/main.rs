fe7d2d336d54e47ca270bffbf4d5c39f51d7a0dd
DongKeun, Kim <skj2366@naver.com>
2024-02-26 13:56:18 +0900

    todo example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Result};
 use serde::{Serialize};
 
+mod api;
+mod models;
+mod repository;
+
 #[derive(Serialize)]
 pub struct Response {
+    // pub status: String,
     pub message: String,
 }
 
@@ -24,10 +29,19 @@ async fn not_found() -> Result<HttpResponse> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| 
+    let todo_db = repository::database::Database::new();
+    let app_data = web::Data::new(todo_db);
+
+    HttpServer::new(move ||
         App::new()
-            .service(healthcheck).default_service(web::route().to(not_found)))
-            .bind(("127.0.0.1", 8080))?
-            .run()
-            .await
+            .app_data(app_data.clone())
+            .configure(api::api::config)
+            .service(healthcheck)
+            .default_service(web::route().to(not_found))
+            .wrap(actix_web::middleware::Logger::default())
+    )
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
+
